0a93661478978addfb172cf9c26b0d1949a27d64
yind <yind@freedom.com>
2017-10-21 22:07:57 +0800

    Add support of print out arguments.

--- TestProject/src/MyTestClass.java
+++ TestProject/src/MyTestClass.java
@@ -4,7 +4,9 @@ public class MyTestClass {
 	public static void main(String[] args) {
 		// can use command line to test
 		System.out.println("The number of arguments: " + args.length);
-		
+		for (String s : args) {
+			System.out.println(s);
+		}
 	}
 
 }
